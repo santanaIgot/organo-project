b92a99ca063a1a0c623157d27627096b7fef579b
santanaIgot <santanaigoor01@gmail.com>
2024-01-11 09:31:07 -0300

    Lista suspensa criada

---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,17 @@
+import "./ListaSuspensa.css";
+
+const ListaSupensa = (props) => {
+  console.log(props.itens);
+  return (
+    <div className="Lista-suspensa">
+      <label>{props.label}</label>
+      <select required = {props.required}>
+        {props.itens.map(item => {
+          return <option key={item}>{item}</option>;
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default ListaSupensa;
