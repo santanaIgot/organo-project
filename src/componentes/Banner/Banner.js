1c8e596a41641219c79e857de9fe7a14532988b5
santanaIgot <santanaigoor01@gmail.com>
2024-01-11 09:29:34 -0300

    Banner criado

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,23 @@
+import './Banner.css'
+
+
+function Banner() {
+
+    // isso não é um arquivo html
+    // isso é um JSX 
+    return(
+        <>
+            <header className='banner'>
+                
+               
+                    <img src="./imagens/banner.png" alt="" />
+             
+                
+            </header>
+            
+            
+        </>
+    )
+}
+
+export default Banner
