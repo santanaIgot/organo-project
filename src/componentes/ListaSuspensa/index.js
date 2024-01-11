7cb47f23ea6e6341f03be77f13afdee36da30069
santanaIgot <santanaigoor01@gmail.com>
2024-01-11 15:48:04 -0300

    Adicionando funcionalidade do evento

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,11 +1,20 @@
+
 import "./ListaSuspensa.css";
 
 const ListaSupensa = (props) => {
-  console.log(props.itens);
+  // console.log(props.itens);
+
+ 
+
+
   return (
     <div className="Lista-suspensa">
       <label>{props.label}</label>
-      <select required = {props.required}>
+      <select 
+        onChange={e => props.aoAlterado(e.target.value)} 
+        required = {props.required}
+        value={props.value}
+      >
         {props.itens.map(item => {
           return <option key={item}>{item}</option>;
         })}
