aaeccd420a7bd849324fd85e07fe60762ca81404
santanaIgot <santanaigoor01@gmail.com>
2024-01-11 15:49:45 -0300

    Função altera valor do input

--- src/componentes/CampoTexto/CampoTexto.js
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -1,11 +1,20 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 
 const CampoTexto = (props) =>{
+    // let valor = ''
+
+    // const [valor,setValor]=useState('')
+    const aoDigitado = (e) => {
+        // setValor (e.target.value);
+        // console.log(valor);
+        props.aoAlterado(e.target.value);
+    }
     return(
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required = {props.obrigatorio} type="text" placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aoDigitado} required = {props.obrigatorio} type="text" placeholder={props.placeholder}/>
         </div>
     )
 }
