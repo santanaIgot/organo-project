e9675a7c368ff819292fbc94ac330b7750322db4
santanaIgot <santanaigoor01@gmail.com>
2024-01-11 09:30:34 -0300

    Campo de texto criado

---
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -0,0 +1,14 @@
+import './CampoTexto.css'
+
+
+const CampoTexto = (props) =>{
+    return(
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input required = {props.obrigatorio} type="text" placeholder={props.placeholder}/>
+        </div>
+    )
+}
+
+
+export default CampoTexto
